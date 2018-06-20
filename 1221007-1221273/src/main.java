a083f1c47c82f11a30d97621c836fb8a60bf5646
Helvets <pedro.am.gomes@hotmail.com>
2018-06-20 14:51:19 -0300

    mudanças

--- 1221007-1221273/src/main.java
+++ 1221007-1221273/src/main.java
@@ -1,14 +1,14 @@
 
+import javax.swing.JFrame;
+
 import control.Controller;
 import window.GameWindow;
+import window.Menu;
 
 public class main {
 
 		public static void main (String [] args) {
-			Controller controller = Controller.getController();
-			GameWindow w= new GameWindow(controller);
-			w.setTitle("Xadrez");
-			w.setVisible(true);
-			w.setLayout(null);
+			// initializes menu 
+			Menu menu = new Menu("Escolha uma opção");
 		}
 }
